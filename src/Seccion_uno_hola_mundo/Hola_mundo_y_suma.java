f88f691a055c8c9903c9152c1b1b5be89ccc371b
Luisda <luisda915@gmail.com>
2024-10-17 13:44:30 -0300

    Proband actualiacion elimandno comentarios y agregando println

---
+++ src/Seccion_uno_hola_mundo/Hola_mundo_y_suma.java
@@ -0,0 +1,39 @@
+package Seccion_uno_hola_mundo;
+
+import java.util.Scanner;
+
+public class Hola_mundo_y_suma {
+	public static void main(String[]arg) {
+	 
+	 //Se agrega print
+	   System.out.println("Hola Mundo. Clasico Elegante.");
+  System.out.println("");
+  System.out.println("Ahora veremos como hacer una suma simple de dos numeros enteros");
+  System.out.println("");
+
+//Se declaran variables.
+	int numeroUno, numeroDos , resultado;
+//se inicia el escaner para poder escribir en consola 
+	Scanner sc = new Scanner(System.in);
+
+	  System.out.println("Agrega en la cosola un numero entero");
+	numeroUno = sc.nextInt();//se referencia la variable y se referencia con el escaner
+	  
+	System.out.println("Agrega el otro numero entero para sumarlo");
+	numeroDos = sc.nextInt();
+	
+	resultado = numeroUno + numeroDos;
+
+
+
+  System.out.println("El resultado de la suma de  "+numeroUno +" y "+numeroDos+ " es :"+ resultado);
+ 
+     sc.close();
+ 
+
+	}
+}
+
+/*Parte del video seccion 1 donde escribimos el clasico "Hola Mundo" y donde tambien instanciamos 
+una variable llamada scanner que hace que uno escriba en la consola 
+*/
