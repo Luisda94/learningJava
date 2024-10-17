4338457a387a32b4a7b519dd994c9009eab00190
Luisda <luisda915@gmail.com>
2024-10-17 19:23:15 -0300

    Se indica que se usa if-else

--- Ejercicios/DifenciandoTriangulos.java
+++ Ejercicios/DifenciandoTriangulos.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class DifenciandoTriangulos {
 
 	public static void main(String[] args) {
-
+//Usando condicionales if if-else else
+		
 //lo de aqui es para saber los lados de un triangulo, esto usando condicionales 
 		        Scanner input = new Scanner(System.in);
 		        System.out.println("Ingrese los tres lados del triángulo:");
@@ -14,6 +15,7 @@ public class DifenciandoTriangulos {
 		        if (lado1 + lado2 > lado3 && lado1 + lado3 > lado2 && lado2 + lado3 > lado1) {
 		            if (lado1 == lado2 && lado2 == lado3) 
 		                System.out.println("El triángulo es equilátero.");
+		            
 		            else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3)
 		                System.out.println("El triángulo es isósceles.");
 		            else 
