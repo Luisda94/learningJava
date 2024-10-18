c20eba404d5a8cc2afb6bef6ef416685bfbe50e1
Luisda <luisda915@gmail.com>
2024-10-17 21:52:24 -0300

    Se genera auto-constructor

--- Aprendiendo_POO/TelefonoMovil.java
+++ Aprendiendo_POO/TelefonoMovil.java
@@ -5,5 +5,18 @@ public class TelefonoMovil {
 	private String modelo;
 	private int numeroCamara;
 	private String color;
+	/**
+	 * @param marca
+	 * @param modelo
+	 * @param numeroCamara
+	 * @param color
+	 */
+	public TelefonoMovil(String marca, String modelo, int numeroCamara, String color) {
+		super();
+		this.marca = marca;
+		this.modelo = modelo;
+		this.numeroCamara = numeroCamara;
+		this.color = color;
+	}
 
 }
