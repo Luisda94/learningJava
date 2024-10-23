6c5ce57301efd752889010c6e96b5c76449a4f6f
Luisda <luisda915@gmail.com>
2024-10-23 16:01:09 -0300

    micro correciones

--- src/Ciclos/Contador_Ceros_Positivos_Negativos.java
+++ src/Ciclos/Contador_Ceros_Positivos_Negativos.java
@@ -3,7 +3,7 @@ package Ciclos;
 import java.util.Scanner;
 
 public class Contador_Ceros_Positivos_Negativos {
-
+//ejercicio de contro de flujo
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner Escaneador = new Scanner(System.in);
@@ -14,7 +14,7 @@ public class Contador_Ceros_Positivos_Negativos {
 		int limite = 10; 
 		//este for me cuenta y con el limete no pasa de 10
 		for(int i = 1; i <= limite; i++) {
-			System.out.println("Introduce un número " +i+ " :");
+			System.out.println("Introduce un número " +i+"):");
 			//numero aqui para que sea ingresado solo hasta10 veces
 				int numero = Escaneador.nextInt();
 			//if-else para contar y los numeros positivos la cantidad de ceros y los negativos
