bbde765d3a28972817c79f943f67f92d0da113e3
Luisda <luisda915@gmail.com>
2024-10-24 16:23:39 -0300

    Probando la clase math de forma muy general.

--- String y Math/Seccion_6_Math_y_String/viendo_math.java
+++ String y Math/Seccion_6_Math_y_String/viendo_math.java
@@ -1,6 +1,8 @@
 package Seccion_6_Math_y_String;
 
 public class viendo_math {
+	//la clase math
+	//probando las constante
 	public static final double E = 2.7182818284590452354;
 	public static final double PI = 3.14159265358979323846;
 	public static void main(String[] args) {
@@ -8,7 +10,23 @@ public class viendo_math {
 
 		System.out.println("Pi es: " + Math.PI);
 		System.out.println("E es: " + Math.E);
-
+		//probando metodos de math
+		int a = 2;
+		int b = 8;
+		System.out.println();
+		System.out.println(Math.abs(a)+"Valor Absoluto");
+		
+		System.out.println();
+		System.out.println(Math.max(a, b)+"Valor máximo");
+		System.out.println();
+		System.out.println(Math.min(a, b)+"Valor máximo");
+		System.out.println();
+		System.out.println(Math.sqrt(a)+"Raiz cuadrada");
+		System.out.println();
+		System.out.println(Math.pow(a, b)+"Potencia");
+		double c = 15.862;
+		System.out.println();
+		System.out.println(Math.round(c)+"redondeo");
 			
 	}
 
