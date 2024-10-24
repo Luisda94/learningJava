0764d9c2475fef6a66f33e727190521c65056043
Luisda <luisda915@gmail.com>
2024-10-24 16:01:26 -0300

    probando metodos que convierten string

--- String y Math/Seccion_6_Math_y_String/algunosMetodos.java
+++ String y Math/Seccion_6_Math_y_String/algunosMetodos.java
@@ -5,6 +5,7 @@ public class algunosMetodos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//aqui creamos el objeto cadena , Luego le asignamos sus caracteres.
+
 		String cadena = new String("Mi primera cadena");
 		//luego probamos uno de los metodos que sirve para ignorar mayusculas y minusculas.
 		System.out.println(cadena.equalsIgnoreCase(cadena));
@@ -12,10 +13,27 @@ public class algunosMetodos {
 		//startWith es boleano, nos dice si al principio tiene los mismo caracteres o no.
 		System.out.println(cadena.startsWith("Mi"));
 		
+		
+		
 		String cadena1 = "Mi segunda cadena";
 		//cuenta la cantidad de espacios y caracteres que tiene una cadena.
 		System.out.println(cadena1.length());
 		
+		//nos permite convertir un dato primitivo en String.
+		int diez = 10;
+		String cadena2 = String.valueOf(diez);
+		System.out.println(cadena2);
+		
+
+		//Parseo a Entero
+		String numeroEntero = "5";
+		int cinco = Integer.parseInt(numeroEntero);
+		System.out.println(cinco);
+		
+		//Parseo a Double
+		String numeroDecimal = "2.5";
+		double decimal = Double.parseDouble(numeroDecimal);
+		System.out.println(decimal);
 
 
 	}
