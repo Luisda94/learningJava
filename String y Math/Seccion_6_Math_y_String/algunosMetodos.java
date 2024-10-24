1e35b38a74efbf7a5144c3202f2a3ba5131cd401
Luisda <luisda915@gmail.com>
2024-10-24 15:46:30 -0300

    Probando metodo que ignora mayusculas y minicuscula

--- String y Math/Seccion_6_Math_y_String/algunosMetodos.java
+++ String y Math/Seccion_6_Math_y_String/algunosMetodos.java
@@ -4,7 +4,13 @@ public class algunosMetodos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//aqui creamos el objeto cadena , Luego le asignamos sus caracteres.
+		String cadena = new String("Mi primera cadena");
+		//luego probamos uno de los metodos que sirve para ignorar mayusculas y minusculas.
+		System.out.println(cadena.equalsIgnoreCase(cadena));
+		
+		String cadena1 = "Mi segunda cadena";
+		
 	}
 
 }
