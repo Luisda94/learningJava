e07cce3fe6c325f4effe42881b1d47b0b09b8552
Luisda <luisda915@gmail.com>
2024-10-24 20:04:15 -0300

    Se instancia una Persona con exito

--- Credenciales_usuario_Seccion_6/modelo_persona/Aplicacion_de_credenciales.java
+++ Credenciales_usuario_Seccion_6/modelo_persona/Aplicacion_de_credenciales.java
@@ -4,7 +4,12 @@ public class Aplicacion_de_credenciales {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
+		Persona persona1 = new Persona("Miguel Canceso","Contraseña");
+		
+		System.out.println(persona1);
+		
 	}
 
 }
