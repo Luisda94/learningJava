4dad8e5393d3e59a3280abb3297a3374e53aad49
Luisda <luisda915@gmail.com>
2024-10-24 15:52:02 -0300

    prueba metodo StarWtih

--- String y Math/Seccion_6_Math_y_String/algunosMetodos.java
+++ String y Math/Seccion_6_Math_y_String/algunosMetodos.java
@@ -9,9 +9,15 @@ public class algunosMetodos {
 		//luego probamos uno de los metodos que sirve para ignorar mayusculas y minusculas.
 		System.out.println(cadena.equalsIgnoreCase(cadena));
 		
+		//startWith es boleano, nos dice si al principio tiene los mismo caracteres o no.
+		System.out.println(cadena.startsWith("Mi"));
+		
 		String cadena1 = "Mi segunda cadena";
 		//cuenta la cantidad de espacios y caracteres que tiene una cadena.
 		System.out.println(cadena1.length());
+		
+
+
 	}
 
 }
