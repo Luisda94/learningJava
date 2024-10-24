644649a2391244936b53618a297fa1ccfad24f92
Luisda <luisda915@gmail.com>
2024-10-24 16:17:06 -0300

    probando constantes

---
+++ String y Math/Seccion_6_Math_y_String/viendo_math.java
@@ -0,0 +1,15 @@
+package Seccion_6_Math_y_String;
+
+public class viendo_math {
+	public static final double E = 2.7182818284590452354;
+	public static final double PI = 3.14159265358979323846;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Pi es: " + Math.PI);
+		System.out.println("E es: " + Math.E);
+
+			
+	}
+
+}
