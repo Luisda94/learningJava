66a76c93cb97070eeb28545a9866778590a0db9f
Luisda <luisda915@gmail.com>
2024-10-24 21:33:54 -0300

    Se crea metodos Math para raiz, ver Pi y calcular potencia.

--- Credenciales_usuario_Seccion_6/modelo_persona/OperacionesMatematicas.java
+++ Credenciales_usuario_Seccion_6/modelo_persona/OperacionesMatematicas.java
@@ -1,5 +1,19 @@
 package modelo_persona;
 
 public class OperacionesMatematicas {
+    // Método para calcular la raíz cuadrada
+	// Aplicando Math
+    public double calcularRaizCuadrada(double numero) {
+        return Math.sqrt(numero);
+    }
 
+    // Método para obtener el valor de Pi
+    public double obtenerValorPi() {
+        return Math.PI;
+    }
+
+    // Método para calcular la potencia de un número
+    public double calcularPotencia(double base, double exponente) {
+        return Math.pow(base, exponente);
+    }
 }
